26d2fb63b3c5c71caae16851e1d2235eb378f523
asthasingh2445@gmail.com <asthasingh2445@gmail.com>
2023-11-02 22:47:57 +0530

    fix the format of InvalidFormatException

--- src/main/java/com/singh/astha/notificationservice/exceptions/handler/InvalidFormatExceptionHandler.java
+++ src/main/java/com/singh/astha/notificationservice/exceptions/handler/InvalidFormatExceptionHandler.java
@@ -2,14 +2,12 @@ package com.singh.astha.notificationservice.exceptions.handler;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.singh.astha.notificationservice.dtos.response.ResponseWrapper;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.HashMap;
 
 @ControllerAdvice
@@ -18,8 +16,11 @@ public class InvalidFormatExceptionHandler {
     @ExceptionHandler(InvalidFormatException.class)
     public ResponseEntity<ResponseWrapper<Object>> handleInvalidFormatException(HttpServletRequest httpServletRequest,
                                                                                 InvalidFormatException exception) {
-        ResponseWrapper<Object> responseWrapper = ResponseWrapper.failure(exception.getValue(),
-                exception.getOriginalMessage());
-        return new ResponseEntity<>(responseWrapper, HttpStatus.BAD_REQUEST);
+        HashMap<String, String> errors = new HashMap<>();
+        if (exception.getTargetType().isEnum()) {
+            String validValues = Arrays.toString(exception.getTargetType().getEnumConstants());
+            errors.put(exception.getPath().get(0).getFieldName(), String.format("Invalid enum value. Valid values are: %s", validValues));
+        }
+        return ResponseEntity.badRequest().body(ResponseWrapper.failure(errors));
     }
 }
