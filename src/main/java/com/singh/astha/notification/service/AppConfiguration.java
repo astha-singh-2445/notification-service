37f6799afdb0a2ce01c02484659c3694b5d239c5
asthasingh2445@gmail.com <asthasingh2445@gmail.com>
2023-12-07 17:43:43 +0530

    move topic field to application properties

--- src/main/java/com/singh/astha/notification/service/AppConfiguration.java
+++ src/main/java/com/singh/astha/notification/service/AppConfiguration.java
@@ -3,6 +3,7 @@ package com.singh.astha.notification.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.singh.astha.notification.service.utils.Constants;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -23,9 +24,18 @@ public class AppConfiguration {
 
     private static final List<String> SCOPES = List.of(Constants.MESSAGING_SCOPE);
 
+    @Value("${kafka.topic.name}")
+    private String kafkaTopicName;
+
+    @Value("${kafka.topic.partitions}")
+    private String kafkaTopicPartitions;
+
+    @Value("${kafka.topic.replicas}")
+    private String kafkaTopicReplicas;
+
     @Bean
     public NewTopic ingestionTopic() {
-        return TopicBuilder.name(Constants.NOTIFICATION_INGESTION).partitions(8).replicas(1).build();
+        return TopicBuilder.name(kafkaTopicName).partitions(Integer.parseInt(kafkaTopicPartitions)).replicas(Integer.parseInt(kafkaTopicReplicas)).build();
     }
 
     @Bean
